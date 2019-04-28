2cfd93d611bcfda9746e91a3fe2a35e1495bf782
王剑峰 <remixknighx@sina.cn>
2019-04-28 22:24:21 +0800

    add

---
+++ exercise/leetcode/longest_univalue_path.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+687. Longest Univalue Path
+@link https://leetcode.com/problems/longest-univalue-path/
+"""
+
+class Solution:
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        return 0
